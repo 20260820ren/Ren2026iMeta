badfa049f2151121095a49929c195bd0e6906bbf
20260820ren <feier201000@163.com>
2026-09-07 20:31:58 +0800

    Add files via upload

---
+++ Figure 2/Figure 2B-2D/Heatmap/juicer.sh
@@ -0,0 +1,1 @@
+sh juicer.sh -g Ttemminckii -d juicer_dir -s DpnII -S early -p Ttemminckii.len -y Ttemminckii_DpnII.txt -z Ttemminckii.fasta -D juicer-1.6 -t 24
---
+++ Figure 2/Figure 2B-2D/Heatmap/run.3d_dna.sh
@@ -0,0 +1,1 @@
+sh run-asm-pipeline.sh Ttemminckii.fasta merged_nodups.txt
